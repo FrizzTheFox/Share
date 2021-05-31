fb834ddc9e2b7356750877a9f930ae53726f7be6
Jawad AHMIMID <jahmimid@e2r6p10.42.fr>
2021-05-31 17:17:20 +0200

    Norminette libft.h

--- libft.h
+++ libft.h
@@ -1,10 +1,10 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h> 
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h> 
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5000
@@ -14,7 +14,7 @@
 #  define OPEN_MAX 32
 # endif
 
-typedef struct		s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
