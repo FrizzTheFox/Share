7a910f7e5ad85badc0d3df63ba6b3aa06c14c92b
Jawad AHMIMID <jahmimid@e2r6p10.42.fr>
2021-06-14 16:40:49 +0200

    finish split, testers not ok yet

--- ft_split.c
+++ ft_split.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jahmimid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/07 16:57:03 by jahmimid          #+#    #+#             */
-/*   Updated: 2021/06/10 16:47:52 by jahmimid         ###   ########.fr       */
+/*   Created: 2021/06/14 12:12:53 by jahmimid          #+#    #+#             */
+/*   Updated: 2021/06/14 16:28:52 by jahmimid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static int		ft_needed_strs(char const *s, char c)
+static int	ft_word_count(char *s, char c)
 {
-	int	i;
-	int	count;
+	int i;
+	int count;
 
 	i = 0;
 	count = 0;
+	if (s == NULL || s == '\0')
+		return (0);
 	while (s[i] != '\0')
 	{
 		if (s[i] == c && s[i + 1] != c)
@@ -30,72 +32,88 @@ static int		ft_needed_strs(char const *s, char c)
 	return (count);
 }
 
-static char		*ft_word_alloc(char *s, char c)
-{
-	int		i;
-	char	*str;
-
-	i = 0;
-	while(s[i] != c && s[i] != '\0')
-		i++;
-	str = (char *)malloc(sizeof(char) * (i + 1));
-	if (str = NULL)
-		return (NULL);
-	return (str);
-	
-}
-
-static char	*ft_word_fill(char *s1, char *s2, char c)
+static int	ft_word_lenght(char *s, char c)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] != c && s1[i] != '\0')
-	{
-		
-	}
+	if (s == NULL || s == '\0')
+		return (0);
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	return (i);
 }
 
-char	**ft_split(char const *s, char c)
-{
-	char	**str;
-	char	*scpy;
-	int		count;
-	int		i;
-	int		j;
-	int		k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	count = ft_needed_strs(s, c);
-	str = (char **)malloc(sizeof(char *) * (count + 1));
-	if (str == NULL)
-		return (NULL);
-	while (count > 0)
-	{
-		str[i][j] = s[k];
-		if (s[k] == c)
-		{
-			i++;
-			j = 0;
-		}
-		j++;
-		k++;
-	}
-	return (str);
-}
-
-int		main(void)
+static void	ft_strcpy(char *src, char *dst, int len)
 {
 	int i;
 
 	i = 0;
-	char **str = ft_split("This is the worst sentance ever written by men", ' ');
-	while (i < 9)
+	while (src[i] != '\0' && i < len)
 	{
-		printf("%s \n", str[i]);
+		dst[i] = src[i];
 		i++;
 	}
+	dst[i] = '\0';
+}
+
+/*static void	**ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str[i]);
+	return (NULL);
+}*/
+
+char	**ft_split(char const *s, char c)
+{
+	int	j;
+	char **str;
+
+	j = 0;
+	if (!s)
+		return (NULL);
+	str = malloc(sizeof(char *) * (ft_word_count((char *)s, c) + 1));
+	if (str == NULL)
+		return (NULL);
+	str[ft_word_count((char *)s, c) + 1] = NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			s++;
+		else
+		{
+			str[j] = malloc(sizeof(char) * (ft_word_lenght((char *)s, c) + 1));
+			if (str[j] == NULL)
+			{
+//				ft_free(str);
+				return (NULL);
+			}
+			ft_strcpy((char *)s, str[j], ft_word_lenght((char *)s, c));
+			j++;
+			s = s + ft_word_lenght((char *)s, c);
+		}
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	int i;
+
+	i = 0;
+	char **str = ft_split("      split       this for   me  !      ", ' ' );
+	while (i < 5)
+	{
+		printf("ft_split %s\n", str[i]);
+		i++;
+	}
+
 	return 0;
 }
