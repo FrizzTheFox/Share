cbb0ea392218c9f2f0ea0a4e213ed1f017f66e8c
Frizz <jawad_ah@hotmail.fr>
2021-06-30 14:03:48 +0200

    Finished all functions

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,5 +2,30 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_list;
+	t_list	*new_element;
 	
+	if (!f || !del)
+		return (NULL);
+	new_list = NULL;
+	new_element = ft_lstnew(f(lst->content));
+	if (new_element == NULL)
+	{
+		ft_lstclear(&new_list, del);
+		return (NULL);
+	}
+	new_list = new_element;
+	lst = lst->next;
+	while (lst)
+	{
+		new_element = ft_lstnew(f(lst->content));
+		if (new_element == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+	ft_lstadd_back(&new_list, new_element);
+	lst = lst->next;
+	}
+	return (new_list);
 }
