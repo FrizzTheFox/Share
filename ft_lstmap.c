1c9e7af8ca38ddf89e75f973079ad5202f005776
Jawad AHMIMID <jahmimid@e2r3p5.42.fr>
2021-07-01 19:58:46 +0200

    lstmap update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,30 +2,29 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*new_element;
+	t_list	*new;
+	t_list	*beg;
+	t_list	*add;
 
-	if (!f || !del)
-		return (NULL);
-	new_list = NULL;
-	new_element = ft_lstnew(f(lst->content));
-	if (new_element == NULL)
+	if (lst)
 	{
-		ft_lstclear(&new_list, del);
-		return (NULL);
-	}
-	new_list = new_element;
-	lst = lst->next;
-	while (lst)
-	{
-		new_element = ft_lstnew(f(lst->content));
-		if (new_element == NULL)
-		{
-			ft_lstclear(&new_list, del);
+		new = lst;
+		beg = ft_lstnew(f(lst->content));
+		if (beg == NULL)
 			return (NULL);
+		new = new->next;
+		while (new)
+		{
+			add = ft_lstnew(f(new->content));
+			if (add == NULL)
+			{
+				ft_lstclear(&beg, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&beg, add);
+			new = new->next;
 		}
-		ft_lstadd_back(&new_list, new_element);
-		lst = lst->next;
+		return (beg);
 	}
-	return (new_list);
+	return (NULL);
 }
